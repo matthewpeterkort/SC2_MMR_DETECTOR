26bf5b5b797a7298f6cb558954b115a5ccfcc419
matthewpeterkort <33436238+matthewpeterkort@users.noreply.github.com>
2022-07-08 20:59:23 -0700

    Add files via upload
    changed the preprocessing so that it upscales directly after the initial crop to add white pixels for better detection accuracy

--- opponent_detector/MMRDETEC.py
+++ opponent_detector/MMRDETEC.py
@@ -38,36 +38,38 @@ while(1):
 	
 	img = pyautogui.screenshot()
 	image = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-	#cv2.imwrite("in_memory_to_disk"+str(imagenumber)+".png", img)
+	#cv2.imwrite("in_memory_to_disk"+str(imagenumber)+".png", image)
 	#image = cv2.imread("in_memory_to_disk"+str(imagenumber)+".png")
-	#image = cv2.imread("examples"+".png")
-	
+	#image = cv2.imread("tester"+".png")
+	#image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
 	crop_img = image[465:490, 1400:1450]
-	gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-	retOne,thresh1 = cv2.threshold(gray,80,255,cv2.THRESH_BINARY)
-	resized = cv2.resize(thresh1, (100,100), interpolation = cv2.INTER_LINEAR             )
+	resized = cv2.resize(crop_img, (100,100), interpolation = cv2.INTER_CUBIC  )
+	new = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+	retOne,resized = cv2.threshold(new,80,255,cv2.THRESH_BINARY)
 
 
 	cropt_img_partTwo = image[442:469, 1461:1669]
-	#gray_two = cv2.cvtColor(cropt_img_partTwo, cv2.COLOR_BGR2GRAY)
-	retTwo,thresh3 = cv2.threshold(cropt_img_partTwo,66,255,cv2.THRESH_BINARY)
-	resized_TWO = cv2.resize(thresh3,  (500,45), interpolation = cv2.INTER_NEAREST             )
-		
+	resized_TWO_TWO= cv2.resize(cropt_img_partTwo,  (200,45), interpolation = cv2.INTER_CUBIC             )
+	gray_two = cv2.cvtColor(resized_TWO_TWO, cv2.COLOR_BGR2GRAY)
+	retTwo,resized_TWO = cv2.threshold(gray_two,66,255,cv2.THRESH_BINARY)
+
+
 	crop_img_two = image[442:469, 251:459]
-	gray_three = cv2.cvtColor(crop_img_two, cv2.COLOR_BGR2GRAY)
-	retThree,thresh2 = cv2.threshold(gray_three,66,255,cv2.THRESH_BINARY)
-	resized_THREE = cv2.resize(thresh2,  (500,45), interpolation = cv2.INTER_NEAREST             )
+	resized_THREE_THREE = cv2.resize(crop_img_two,  (200,45), interpolation = cv2.INTER_NEAREST             )
+	gray_three = cv2.cvtColor(resized_THREE_THREE, cv2.COLOR_BGR2GRAY)
+	retThree,resized_THREE = cv2.threshold(gray_three,66,255,cv2.THRESH_BINARY)
 
 	crop_img_two_parttwo = image[465:490, 470:520]
-	#gray_two_parttwo = cv2.cvtColor(crop_img_two_parttwo, cv2.COLOR_BGR2GRAY)
-	retFour,thresh4 = cv2.threshold(crop_img_two_parttwo,80,255,cv2.THRESH_BINARY)
-	resized_two = cv2.resize(thresh4, (100,100), interpolation = cv2.INTER_LINEAR              )
+	resized_two_two = cv2.resize(crop_img_two_parttwo, (100,100), interpolation = cv2.INTER_LINEAR              )
+	gray_two_parttwo = cv2.cvtColor(resized_two_two, cv2.COLOR_BGR2GRAY)
+	retFour,resized_two_two = cv2.threshold(gray_two_parttwo,80,255,cv2.THRESH_BINARY)
 
 	
 	#cv2.imshow("nesks",resized)
-	#cv2.imshow("crop_two",resized)
-	#cv2.imshow("crop_four",resized_two)
-	#cv2.imshow("Test_img.jpg",resized_TWO)
+	#cv2.imshow("crop_two",resized_TWO)
+	#cv2.imshow("crop_four",resized_THREE)
+	#cv2.imshow("Test_img.jpg",resized_two_two)
 	#cv2.waitKey(0)
 	#cv2.destroyAllWindows()
 
@@ -75,7 +77,7 @@ while(1):
 	Right_Word= pytesseract.image_to_string(resized_TWO, lang='eurostile', config='--psm 7')
 	
 	Left_Word= pytesseract.image_to_string(resized_THREE,lang="eurostile", config='--psm 7' )
-	Left_Num= pytesseract.image_to_string(resized_two,lang="eurostile", config='digits')
+	Left_Num= pytesseract.image_to_string(resized_two_two,lang="eurostile", config='digits')
 
 
 	new_Right_Num = ""
@@ -136,6 +138,8 @@ while(1):
 			replace_line('Current_Oponnent.txt', 0,  'Protoss IIIIIIIII ' + str(new_Left_Num) )
 
 	else:
+		
+		
 		Eury_string = "Eurystheus"
 		if(Eury_string in Right_Word):
 			if(np.all((race_crop_left - left_terran) == 0)):
@@ -145,6 +149,7 @@ while(1):
 			if(np.all((race_crop_left - left_protoss) == 0)):
 				replace_line('Current_Oponnent.txt', 0,  'Protoss ' + new_Left_Word + " " + new_Left_Num )
 		else:
+			
 			if(np.all((race_crop_right - right_terran) == 0)):
 				replace_line('Current_Oponnent.txt', 0,  'Terran ' + new_Right_Word + " " + new_Right_Num )
 			if(np.all((race_crop_right - right_zerg) == 0)):
@@ -152,8 +157,7 @@ while(1):
 			if(np.all((race_crop_right - right_protoss) == 0)):
 				replace_line('Current_Oponnent.txt', 0,  'Protoss ' + new_Right_Word + " " + new_Right_Num )
 
-		
-	time.sleep(3)
+	
 
 
 
